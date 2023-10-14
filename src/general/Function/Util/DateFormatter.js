e950f4e06781899ca301d7109e695ed257992a60
anisirji <www.allinoneforyouhere@gmail.com>
2023-10-14 09:58:41 +0530

    s

--- src/general/Function/Util/DateFormatter.js
+++ src/general/Function/Util/DateFormatter.js
@@ -2,7 +2,6 @@ const moment = require("moment");
 
 function formatDate(userInputDate) {
   const parsedDate = moment(userInputDate, "D-M-YYYY", true);
-
   return parsedDate.isValid() ? parsedDate.format("YYYY-MM-DD") : null;
 }
 
--- src/general/Function/Util/OrderID.js
+++ src/general/Function/Util/OrderID.js
@@ -1,19 +1,27 @@
-function generateOrderID(prefix, lastOrderId, suffix = '') {
-    if (lastOrderId === undefined) {
-        lastOrderId = 0; // Define the first order ID as 0
-    } else {
-        // Extract the numeric part of the last order ID and increment it
-        const lastOrderNumericPart = lastOrderId.match(/\d+/);
-        lastOrderId = lastOrderNumericPart ? parseInt(lastOrderNumericPart[0]) + 1 : 0;
-    }
+function generateOrderID(lastOrderId, prefix, suffix = "") {
+  if (lastOrderId === undefined) {
+    lastOrderId = 0; // Define the first order ID as 0
+  } else {
+    // Extract the numeric part of the last order ID and increment it
+    const lastOrderNumericPart = lastOrderId.match(/\d+/);
+    lastOrderId = lastOrderNumericPart
+      ? parseInt(lastOrderNumericPart[0]) + 1
+      : 0;
+  }
 
-    lastOrderId = lastOrderId < 1 ? 1 : lastOrderId; // Ensure the minimum value is 1
+  lastOrderId = lastOrderId < 1 ? 1 : lastOrderId; // Ensure the minimum value is 1
 
-    const formattedOrderId = `${prefix}-${String(lastOrderId).padStart(7, '0')}-${suffix}`;
-    return formattedOrderId;
-    // You can save the last order ID to a database to ensure uniqueness across server restarts.
-    // For simplicity, we'll keep it in memory.
+  const formattedOrderId = `${prefix}-${String(lastOrderId).padStart(
+    7,
+    "0"
+  )}-${suffix}`;
+  return formattedOrderId;
+  // You can save the last order ID to a database to ensure uniqueness across server restarts.
+  // For simplicity, we'll keep it in memory.
 }
 
-module.exports = { generateOrderID };
+console.log(generateOrderID(undefined, "ORDER-"));
+console.log(generateOrderID(undefined, "ORDER-", "-Prefix"));
+console.log(generateOrderID("ORDER-00001-SOMETHING", "ORDER-"));
 
+module.exports = { generateOrderID };
